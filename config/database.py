3563d1eaaf27981e46488e443001532af4b63648
Diksha <dikshadohra@gmail.com>
2026-08-23 19:21:56 +0530

    Add PostgreSQL support and requirements

--- config/database.py
+++ config/database.py
@@ -1,12 +1,23 @@
 import os
 import sqlite3
+import psycopg2
+from urllib.parse import urlparse
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 def get_db_connection():
-    # Project ke main directory ka absolute path calculate karna
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    db_path = os.path.join(base_dir, 'database', 'dx_fragrance.db')
-    
-    # Connection banao
-    connection = sqlite3.connect(db_path)
-    connection.row_factory = sqlite3.Row
-    return connection
+    if DATABASE_URL:
+        url = urlparse(DATABASE_URL)
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+        return conn
+    else:
+        db_path = os.path.join("database", "dx_fragrance.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        return conn
